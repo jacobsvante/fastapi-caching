e3b148062e28759a415bae310302a6adc555feb0
Jacob Magnusson <m@jacobian.se>
2020-08-16 13:23:10 +0200

    Add long description to Python project and bump version number

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os.path
+
 from setuptools import find_packages, setup
 
 install_requires = ["fastapi", "cachetools"]
@@ -21,13 +23,20 @@ extras_require["all"] = [
     dep for _, dependencies in extras_require.items() for dep in dependencies
 ]
 
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="fastapi-caching",
-    version="0.1.0",
+    version="0.1.1",
     description="Cache library for FastAPI with tag based invalidation",
     url="https://github.com/jmagnusson/fastapi-caching",
-    autho="Jacob Magnusson",
+    author="Jacob Magnusson",
     author_email="m@jacobian.se",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
     packages=find_packages(".", include=["fastapi_caching"]),
     python_requires=">=3.6",
